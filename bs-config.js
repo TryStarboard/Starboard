f138a7356c7296e90cbd5b6d99b168ab1818aec0
Daiwei <daiweilu123@gmail.com>
2016-01-18 13:58:04 -0500

    Add live-reload

--- bs-config.js
+++ bs-config.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = {
-
+  ui: false,
+  files: [
+    './template/**',
+    './source/**',
+    './config/**',
+  ],
+  proxy: {
+    target: 'http://localhost:10000',
+  },
+  port: 10010,
+  snippetOptions: {
+    rule: {
+      match: /$/
+    }
+  },
+  reloadDebounce: 1000
 };
