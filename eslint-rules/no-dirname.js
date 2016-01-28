7993bdc9128d0469074b64dd5e15da05236e701a
Daiwei <daiweilu123@gmail.com>
2016-01-28 17:28:45 -0500

    Make rules to disable __dirname

---
+++ eslint-rules/no-dirname.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function (context) {
+  return {
+    "Program:exit"() {
+      context.getScope().through.some((ref) => {
+        if (ref.identifier.name === '__dirname') {
+          context.report(ref.identifier, 'Do not use __dirname, it will break server bundle with webpack');
+        }
+      });
+    }
+  };
+}
