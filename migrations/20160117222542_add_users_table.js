05584e49481fae4e0a836ae46e6e5fdb781e494c
Daiwei <daiweilu123@gmail.com>
2016-01-28 22:30:04 -0500

    Update migration

--- migrations/20160117222542_add_users_table.js
+++ migrations/20160117222542_add_users_table.js
@@ -3,8 +3,11 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments();
+    table.bigInteger('github_id');
     table.string('email');
-    table.string('hash');
+    table.string('username');
+    table.string('access_token');
+    table.string('refresh_token');
     table.timestamps();
   });
 };
