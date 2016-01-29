3cc9e6aeea5fac13ca3accfc996a28508fd48630
Daiwei <daiweilu123@gmail.com>
2016-01-28 22:31:00 -0500

    Update UI to use github login

--- source/universal/components/Login.js
+++ source/universal/components/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button } from './Form';
 import { collect } from '../utils/form';
 import { login } from '../actions';
 import octocatImg from '../../client/img/octocat.jpg';
@@ -10,14 +9,9 @@ class Login extends Component {
     return (
       <div className="login">
         <img className="login__github-logo" src={octocatImg}></img>
-        <Button>Login with Github</Button>
+        <a href="/api/v1/login">Login with Github</a>
       </div>
     );
-  }
-
-  _onSubmit(event) {
-    event.preventDefault();
-    this.props.dispatch(login(collect(event.target)));
   }
 }
 
