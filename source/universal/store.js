ff14b7873e237b725dc196e80ae4975769faf3ed
Daiwei <daiweilu123@gmail.com>
2016-01-31 19:41:48 -0500

    Fix redux logger server render issue

--- source/universal/store.js
+++ source/universal/store.js
@@ -8,7 +8,7 @@ import {
 
 const createStoreWithMiddleware = applyMiddleware(
   promiseMiddleware(),
-  createLogger()
+  createLogger({logger: console})
 )(createStore);
 
 const reducers = combineReducers({
