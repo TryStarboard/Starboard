ab70f3f4ecb31115ab348ae286f5d13d33fd3744
Daiwei <daiweilu123@gmail.com>
2016-01-31 19:43:02 -0500

    Add sidebar to UI

--- source/universal/components/Dashboard.js
+++ source/universal/components/Dashboard.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions';
+import LogoutIcon from '../../client/img/logout-icon.svg';
+import UserIcon from '../../client/img/user-icon.svg';
+import DashboardIcon from '../../client/img/dashboard-icon.svg';
+
+const Sidebar = ({dispatch}) => (
+  <nav className='nav'>
+    <div className="nav__top">
+      <button className='nav__btn'><UserIcon/></button>
+      <button className='nav__btn'><DashboardIcon/></button>
+    </div>
+    <div className="nav__bottom">
+      <button className='nav__btn' onClick={() => dispatch(logout())}><LogoutIcon/></button>
+    </div>
+  </nav>
+);
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <div>Dashboard</div>
-        <button onClick={this._logout.bind(this)}>logout</button>
+        <Sidebar {...this.props}></Sidebar>
       </div>
     );
-  }
-
-  _logout() {
-    this.props.dispatch(logout());
   }
 }
 
