b576607cf4b180f4858b33a8b9c5176825dc64a4
Daiwei <daiweilu123@gmail.com>
2016-01-31 23:39:42 -0500

    Create get stars action

--- source/universal/actions/index.js
+++ source/universal/actions/index.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
 export const LOGOUT = 'LOGOUT';
+export const GET_STARS = 'GET_STARS';
 
 export function logout() {
   return {
     type: LOGOUT,
     payload: {
       promise: axios.get('/api/v1/logout')
+    }
+  };
+}
+
+export function getStars() {
+  return {
+    type: GET_STARS,
+    payload: {
+      promise: axios.get('/api/v1/stars')
     }
   };
 }
--- source/universal/components/Dashboard.js
+++ source/universal/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { logout } from '../actions';
+import { logout, getStars } from '../actions';
 import LogoutIcon from '../../client/img/logout-icon.svg';
 import UserIcon from '../../client/img/user-icon.svg';
 import DashboardIcon from '../../client/img/dashboard-icon.svg';
@@ -9,7 +9,7 @@ const Sidebar = ({dispatch}) => (
   <nav className='nav'>
     <div className="nav__top">
       <button className='nav__btn'><UserIcon/></button>
-      <button className='nav__btn'><DashboardIcon/></button>
+      <button className='nav__btn' onClick={() => dispatch(getStars())}><DashboardIcon/></button>
     </div>
     <div className="nav__bottom">
       <button className='nav__btn' onClick={() => dispatch(logout())}><LogoutIcon/></button>
@@ -27,4 +27,4 @@ class Dashboard extends Component {
   }
 }
 
-export default connect((s) => s)(Dashboard);
+export default connect(({stars, user}) => ({stars, user}))(Dashboard);
