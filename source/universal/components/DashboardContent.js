0142df04bdd9e29ad7a806cd900e6da26e0a51ac
Daiwei <daiweilu123@gmail.com>
2016-02-01 21:30:14 -0500

    Display repo base info

--- source/universal/components/DashboardContent.js
+++ source/universal/components/DashboardContent.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const Repo = ({full_name}) => (
-  <div className="">{full_name}</div>
+const Repo = ({full_name, description, html_url}) => (
+  <div className="repo">
+    <div className="repo__full-name">
+      <a className="repo__name-link" target="_blank" href={html_url}>{full_name}</a>
+    </div>
+    <div className="repo__desc">{description}</div>
+  </div>
 );
 
 export default ({stars}) => (
