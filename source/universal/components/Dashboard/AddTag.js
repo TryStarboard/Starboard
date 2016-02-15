a495dc6217db2358ec00297b16e4860e99c5d8d5
Daiwei <daiweilu123@gmail.com>
2016-02-14 21:26:47 -0500

    Hover delete style

--- source/universal/components/Dashboard/AddTag.js
+++ source/universal/components/Dashboard/AddTag.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { DropTarget } from 'react-dnd';
+import classnames from 'classnames';
 import PlusIcon from '../../../client/img/add-tag-icon.svg';
 import TrashCanIcon from '../../../client/img/trash-can-icon.svg';
 
-const AddTag = ({onClick, ui: {isDraggingTag}}) => {
-  return (
-    <button className="tag tag--btn" onClick={onClick}>
-      {isDraggingTag ? <TrashCanIcon/> : <PlusIcon/>}
-    </button>
-  );
-};
+class AddTag extends Component {
+  render() {
+    const {
+      onClick,
+      ui: {isDraggingTag},
+      connectDropTarget,
+      isOver,
+    } = this.props;
 
-export { AddTag as default };
+    return connectDropTarget(
+      <button
+        className={classnames('tag', 'tag--btn', {'tag--delete-active': isOver})}
+        onClick={onClick}>
+        {isDraggingTag ? <TrashCanIcon/> : <PlusIcon/>}
+      </button>
+    );
+  }
+}
+
+export default DropTarget(
+  'TAG',
+  {
+    drop(props, monitor) {
+      const tag = monitor.getItem();
+      console.log(tag);
+      // props.applyTagToRepo(tag.id, props.id);
+    }
+  },
+  (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+  })
+)(AddTag);
--- source/universal/components/Dashboard/DashboardContent.js
+++ source/universal/components/Dashboard/DashboardContent.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import AddTag from './AddTag';
