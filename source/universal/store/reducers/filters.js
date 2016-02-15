61343cda7aa7b68ad55111556dddee6df8e6c238
Daiwei <daiweilu123@gmail.com>
2016-02-15 14:11:49 -0500

    Make unselect tag filter possible

--- source/universal/store/reducers/filters.js
+++ source/universal/store/reducers/filters.js
@@ -1,11 +1,17 @@
 import u from 'updeep';
-import { contains, not, pipe } from 'ramda';
+import { contains } from 'ramda';
 import { SELECT_TAG } from '../../actions/index';
 
 export default function (state = [], { type, payload }) {
   switch (type) {
   case SELECT_TAG:
-    return u(u.if(pipe(contains(payload.id), not), u.constant([payload.id])), state);
+    return u(
+      u.ifElse(
+        contains(payload.id),
+        u.constant([]),
+        u.constant([payload.id])
+      ),
+      state);
   default:
     return state;
   }
