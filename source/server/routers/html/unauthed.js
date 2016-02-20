ae5329b471ce6ab92bcdb1556b0673c375aa9740
Daiwei <daiweilu123@gmail.com>
2016-02-20 13:45:11 -0500

    Handle error generated during github callback

--- source/server/routers/html/unauthed.js
+++ source/server/routers/html/unauthed.js
@@ -1,8 +1,9 @@
 import Router from 'koa-router';
 import { fromCallback } from 'bluebird';
 import { createLoginUrl, handleLoginCallback } from '../../util/github';
-import renderReact from '../util/renderReact';
+import { logger } from '../../util/logging';
 import { fetchUserProfile, upsert as upsertUser } from '../../model/user';
+import renderReact from '../util/renderReact';
 
 const unauthedRoute = new Router();
 
@@ -16,23 +17,23 @@ function *ensureUnauthed(next) {
 
 unauthedRoute.get('/login', ensureUnauthed, renderReact);
 
-unauthedRoute.get(
-  '/github-login',
-  ensureUnauthed,
-  function *(next) {
-    this.redirect(createLoginUrl());
-  }
-);
+unauthedRoute.get('/github-login', ensureUnauthed, function *(next) {
+  this.redirect(createLoginUrl());
+});
 
-unauthedRoute.get('/github-back',
-  ensureUnauthed,
-  function *(next) {
+unauthedRoute.get('/github-back', ensureUnauthed, function *(next) {
+  try {
     const access_token = yield handleLoginCallback(this.query);
     const user = yield fetchUserProfile(access_token);
     const id = yield upsertUser(user, access_token);
     yield fromCallback((done) => this.req.login({ id }, done));
     this.redirect('/dashboard');
+  } catch (err) {
+    logger.error('github auth callback error', {
+      error: err,
+    });
+    this.redirect('/login');
   }
-);
+});
 
 export { unauthedRoute as default };
