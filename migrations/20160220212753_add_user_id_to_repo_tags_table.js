e9b316d6ceb07dafd729da08d4cee8ccf24872ea
Daiwei <daiweilu123@gmail.com>
2016-02-21 10:52:21 -0500

    Fix lint errors

--- migrations/20160220212753_add_user_id_to_repo_tags_table.js
+++ migrations/20160220212753_add_user_id_to_repo_tags_table.js
@@ -1,12 +1,13 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex.schema.table('repo_tags', (table) => {
     table.string('user_id');
-  })
+  });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.table('repo_tags', (table) => {
     table.dropColumn('user_id');
-  })
+  });
 };
