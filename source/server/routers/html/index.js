32e1aeff8ff15f0699420436ff26c02aa1afd893
Daiwei <daiweilu123@gmail.com>
2016-03-03 20:32:37 -0500

    Remove create react app

--- source/server/routers/html/index.js
+++ source/server/routers/html/index.js
@@ -1,3 +1,37 @@
-export default function *(next) {
-  yield this.render('index');
-}
+import Router from 'koa-router';
+
+const router = new Router();
+
+router.get('/', function *() {
+  if (this.req.isAuthenticated()) {
+    this.redirect('/dashboard');
+  } else {
+    this.redirect('/login');
+  }
+});
+
+router.get('/login', function *() {
+  if (this.req.isAuthenticated()) {
+    this.redirect('/dashboard');
+  } else {
+    yield this.render('index');
+  }
+});
+
+router.get('/dashboard', function *() {
+  if (!this.req.isAuthenticated()) {
+    this.redirect('/login');
+  } else {
+    yield this.render('index');
+  }
+});
+
+router.get('/user-profile', function *() {
+  if (!this.req.isAuthenticated()) {
+    this.redirect('/login');
+  } else {
+    yield this.render('index');
+  }
+});
+
+export default router.routes();
