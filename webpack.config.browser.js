966a5f313a9f1ec0b86238f5e6d802d988487384
Daiwei <daiweilu123@gmail.com>
2016-03-08 15:39:18 -0500

    Remove ts-loader

--- webpack.config.browser.js
+++ webpack.config.browser.js
@@ -12,20 +12,8 @@ module.exports = {
 
   devtool: 'source-map',
 
-  resolve: {
-    alias: {
-      img: join(__dirname, './source/client/img'),
-      svg: join(__dirname, './source/client/svg'),
-    }
-  },
-
   module: {
     loaders: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel?presets[]=es2015&presets[]=react!ts'
-      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
