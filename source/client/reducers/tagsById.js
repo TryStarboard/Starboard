5143c2614b2b8820b383e485c6a9b46083327ada
Daiwei <daiweilu123@gmail.com>
2016-03-10 12:01:37 -0500

    Restore logic of adding new tag

--- source/client/reducers/tagsById.js
+++ source/client/reducers/tagsById.js
@@ -1,4 +1,4 @@
-import { indexBy, prop } from 'ramda';
+import { indexBy, prop, assoc } from 'ramda';
 import { UPDATE_TAGS } from '../actions/serverActions';
 import { ADD_TAG, DELETE_TAG, GET_ALL_TAGS } from '../actions/creators';
 
@@ -6,8 +6,9 @@ export default function (state = {}, { type, payload }) {
   switch (type) {
   case `${GET_ALL_TAGS}_FULFILLED`:
     return indexBy(prop('id'), payload.data);
-  // case `${ADD_TAG}_FULFILLED`:
-  //   return [payload.data].concat(state);
+  case `${ADD_TAG}_FULFILLED`:
+    const newTag = payload.data;
+    return assoc(newTag.id, newTag, state);
   // case UPDATE_TAGS:
   //   return payload;
   // case `${DELETE_TAG}_PENDING`:
