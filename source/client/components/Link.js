837ef3c460992c62d0f8175d86ca024093ba866b
Daiwei <daiweilu123@gmail.com>
2016-03-10 13:43:30 -0500

    Fix broken Link

--- source/client/components/Link.js
+++ source/client/components/Link.js
@@ -1,16 +1,15 @@
-import * as React from 'react';
-import { StatelessComponent } from 'react';
-import { navTo } from '../store';
+import React from 'react';
+import { navTo } from '../routes';
 
-const Link: StatelessComponent<{className: string, to: string, children?: any}> = (props) => {
+const Link = (props) => {
   const onClick = (event: Event) => {
     event.preventDefault();
     navTo(props.to);
   };
 
   return (
-    <a className={props.className} href={props.to} onClick={onClick}>
-      {props.children}
+    <a className={ props.className } href={ props.to } onClick={ onClick }>
+      { props.children }
     </a>
   );
 };
