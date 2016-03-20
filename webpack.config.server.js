4fcc6be86b446b5704998c8c44680144aab7c2fb
Daiwei <daiweilu123@gmail.com>
2016-03-20 17:23:53 -0400

    Merge base build config into server build config

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -2,26 +2,13 @@
 
 const fs = require('fs');
 const join = require('path').join;
-const base = require('./webpack.config.base');
 
-const node_modules = fs.readdirSync('node_modules')
-  .filter((x) => x !== '.bin' )
-  .map((x) => new RegExp(`^${x}`));
-
-base.module.loaders[0].query.plugins =
-  base.module.loaders[0].query.plugins.concat([
-    'transform-function-bind',
-  ]);
-
-base.module.loaders[0].query.presets.push('es2015-node5');
-
-module.exports = Object.assign(base, {
+module.exports = {
   target: 'node',
-
   entry: './source/server/index.js',
-
-  externals: node_modules,
-
+  externals: fs.readdirSync('node_modules')
+    .filter((x) => x !== '.bin' )
+    .map((x) => new RegExp(`^${x}`)),
   node: {
     console: false,
     process: false,
@@ -31,10 +18,31 @@ module.exports = Object.assign(base, {
     __dirname: false,
     setImmediate: false,
   },
-
   output: {
     filename: 'server.js',
     path: join(__dirname, 'build'),
     libraryTarget: 'commonjs2',
   },
-});
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015-node5'],
+          plugins: ['transform-function-bind'],
+        }
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'file',
+      },
+      {
+        test: /\.svg$/,
+        loader: 'babel?presets[]=react&presets[]=es2015!svg-react',
+      },
+    ]
+  },
+};
