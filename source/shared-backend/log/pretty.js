c7f99b8168f52975c182090def58c8bb6755c9ca
Daiwei <daiweilu123@gmail.com>
2016-03-22 22:36:10 -0400

    Reduce server logs

--- source/shared-backend/log/pretty.js
+++ source/shared-backend/log/pretty.js
@@ -2,7 +2,16 @@ import es from 'event-stream';
 import { transformLogData } from './util';
 
 const pretty = es.map((data, cb) => {
-  cb(null, `${JSON.stringify(transformLogData(data), null, 4)}\n`);
+  const transformed = transformLogData(data);
+  const {req, err, msg} = transformed;
+
+  if (err) {
+    cb(null, `${msg}:${err}\n`);
+  } else if (req) {
+    cb(null, `${msg}:${req.url}\n`);
+  } else {
+    cb(null, `${JSON.stringify(transformLogData(data), null, 4)}\n`);
+  }
 });
 
 pretty.pipe(process.stdout);
