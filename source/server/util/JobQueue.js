78b7aa80a1e68e8846bc1fa9b2792178e38d17bf
Daiwei <daiweilu123@gmail.com>
2016-03-22 21:03:14 -0400

    Update websocket

--- source/server/util/JobQueue.js
+++ source/server/util/JobQueue.js
@@ -1,10 +1,6 @@
 import kue                      from 'kue';
 import config                   from 'config';
 import Redis                    from 'ioredis';
-import { Observable           } from 'rx';
-import { props                } from 'bluebird';
-import { getReposWithIds      } from '../../shared-backend/model/Repos';
-import { getAll as getAllTags } from '../../shared-backend/model/Tags';
 
 const REDIS_CONFIG = config.get('redis');
 
@@ -16,50 +12,7 @@ const queue = kue.createQueue({
   }
 });
 
-const sub = new Redis(REDIS_CONFIG);
-
 export function enqueueSyncStarsJob(user_id) {
   const job = queue.create('sync-stars', {user_id});
-  const channelName = `sync-stars:user_id:${user_id}`;
-
-  sub.subscribe(channelName, (err, count) => {});
-  sub.unsubscribe(channelName, (err, count) => {});
-
-  sub.on('message', console.log);
-  sub.removeEventListener('message', console.log);
-
   job.save();
-
-  // const source = Observable.create((observer) => {
-  //   /**
-  //   * progress: number, percentage
-  //   * data: Object
-  //   */
-  //   job.on('progress', (progress, event) => {
-  //     switch (event.type) {
-  //     case 'PROGRESS':
-  //       props({
-  //         repos: getReposWithIds(event.repo_ids),
-  //         tags: getAllTags(user_id),
-  //       })
-  //       .then((data) => {
-  //         observer.onNext({type: 'PROGRESS', progress, data});
-  //       });
-  //       break;
-  //     case 'DELETE':
-  //       observer.onNext({type: 'DELETE', progress, data: event.deleted_repo_ids});
-  //       break;
-  //     default:
-  //       // No additional case
-  //     }
-  //   });
-
-  //   job.save((err) => {
-  //     if (err) {
-  //       observer.onError(err);
-  //     }
-  //   });
-  // });
-
-  // return source;
 }
--- source/server/util/websocket.js
+++ source/server/util/websocket.js
@@ -2,7 +2,12 @@ import config                    from 'config';
 import socketio                  from 'socket.io';
 import Cookies                   from 'cookies';
 import co                        from 'co';
+import { curry                 } from 'ramda';
+import { props                 } from 'bluebird';
+import Redis                     from 'ioredis';
 import log                       from '../../shared-backend/log';
+import { getReposWithIds       } from '../../shared-backend/model/Repos';
+import { getAll as getAllTags  } from '../../shared-backend/model/Tags';
 import { SYNC_REPOS            } from '../../shared/action-types';
 import { client as redisClient } from './redis';
 import { enqueueSyncStarsJob   } from './JobQueue';
@@ -12,12 +17,14 @@ import {
   UPDATE_TAGS
 } from '../../client/actions-server/creators';
 
-const KEYS = config.get('cookie.keys');
+const COOKIE_KEYS = config.get('cookie.keys');
+
+const sub = new Redis(config.get('redis'));
 
 function authenticate(socket, next) {
   co(function *() {
 
-    const cookies = new Cookies(socket.request, null, KEYS);
+    const cookies = new Cookies(socket.request, null, COOKIE_KEYS);
     const sid = cookies.get('koa.sid', {signed: true});
 
     const str = yield redisClient.get(`koa:sess:${sid}`);
@@ -34,31 +41,44 @@ function authenticate(socket, next) {
   }).catch(next);
 }
 
-function handleSyncRepos(socket) {
-  return function () {
-    enqueueSyncStarsJob(socket.handshake.user.id)
-      .subscribe(
-        (event) => {
-          switch (event.type) {
-          case 'PROGRESS':
-            const {tags, repos} = event.data;
-            socket.emit(UPDATE_TAGS, tags);
-            socket.emit(UPDATE_SOME_REPOS, repos);
-            break;
-          case 'DELETE':
-            socket.emit(REMOVE_REPOS, event.data);
-            break;
-          default:
-            // No additional case
-          }
-        },
-        (error) => log.error('sync-stars-error', { error })
-      );
-  };
-}
+const handleChannelMessage = curry((socket, user_id, channel, message) => {
+  const event = JSON.parse(message);
+
+  switch (event.type) {
+  case 'UPDATED_ITEM':
+    props({
+      repos: getReposWithIds(event.repo_ids),
+      tags: getAllTags(user_id),
+    })
+    .then(({tags, repos}) => {
+      socket.emit(UPDATE_TAGS, tags);
+      socket.emit(UPDATE_SOME_REPOS, repos);
+    });
+    break;
+  case 'DELETED_ITEM':
+    socket.emit(REMOVE_REPOS, event.deleted_repo_ids);
+    break;
+  default:
+    // No additional case
+  }
+});
 
 function handleConnection(socket) {
-  socket.on(SYNC_REPOS, handleSyncRepos(socket));
+  const user_id = socket.handshake.user.id;
+
+  sub.subscribe(`sync-stars:user_id:${user_id}`, (err) => {
+    if (err) {
+      log.error('SUBSCRIBE_SYNC_STARS_CHANNEL_ERROR', err);
+    }
+  });
+
+  sub.on('message', handleChannelMessage(socket, user_id));
+
+  socket.on(SYNC_REPOS, () => enqueueSyncStarsJob(user_id));
+
+  // TODO: cleanup
+  // sub.unsubscribe(channelName, (err, count) => {});
+  // sub.removeEventListener('message', console.log);
 }
 
 export function createWebsocketServer(server) {
