1c10d0a15be8db3ef2ee7b1ddcab28b728cb455e
Daiwei <daiweilu123@gmail.com>
2016-03-27 15:13:44 -0400

    Add tag action

--- source/client/components/TagsSideBar.js
+++ source/client/components/TagsSideBar.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import observeStore       from '../higher-order-components/observeStore';
-import {getAllTags}       from '../actions';
-import Tag                from './Tag';
+import React, {Component}   from 'react';
+import observeStore         from '../higher-order-components/observeStore';
+import {getAllTags, addTag} from '../actions';
+import Tag                  from './Tag';
 
 const createObserveComponent = observeStore(
   () => ({tags: ['tags']})
@@ -16,10 +16,14 @@ export default createObserveComponent(
     render() {
       return (
         <div className="dashboard__tags">
-          <div className="dashboard__tags-input-wrapper">
-            <input type="text" className="dashboard__tags-input" placeholder="Search or create new tag..." />
-            <div className="dashboard__tags-input-helper-text">Type for search, hit Enter to create new tag</div>
-          </div>
+          <form className="dashboard__tags-input-wrapper" onSubmit={addTag}>
+            <input
+              type="text"
+              name="tag_text"
+              className="dashboard__tags-input"
+              placeholder="Create new tag..."/>
+            <div className="dashboard__tags-input-helper-text">Type tag name, hit Enter to create new tag</div>
+          </form>
           <div className="dashboard__tags-tag-list">
             {this.props.tags.map((id) => <Tag id={id} key={id} />)}
           </div>
