3655d72a4c30acada062161513a4501d157c971f
Daiwei <daiweilu123@gmail.com>
2016-04-03 10:27:01 -0400

    Improve dev mixpanel stub

--- source/client/mixpanel.js
+++ source/client/mixpanel.js
@@ -1,16 +1,22 @@
 /*global MIXPANEL_TOKEN*/
+
 import mixpanel from 'mixpanel-browser';
 
-let mix = mixpanel;
+let mix;
 
 if (MIXPANEL_TOKEN) {
   mixpanel.init(MIXPANEL_TOKEN); // Token is defined in webpack.config
+  mix = mixpanel;
 } else {
-  let mixpanelStub = {};
-  mixpanelStub.identify = function() {};
-  mixpanelStub.track = function() {};
-
-  mix = mixpanelStub;
+  mix = new Proxy({}, {
+    get(target, property, receiver) {
+      return function (...args) {
+        /*eslint-disable no-console*/
+        console.log(`---> calling "${property}" on mixpanel with arguments:\n${JSON.stringify(args, null, 4)}`);
+        /*eslint-enable*/
+      };
+    }
+  });
 }
 
-export { mix as default };
+export {mix as default};
