6cd12f27ba1e1bc5eb5bb883b5dca60b17a7ff8c
Daiwei <daiweilu123@gmail.com>
2016-04-16 15:38:31 -0400

    Create marketing page

--- source/server/routers/html/index.js
+++ source/server/routers/html/index.js
@@ -6,31 +6,23 @@ router.get('/', function *() {
   if (this.req.isAuthenticated()) {
     this.redirect('/dashboard');
   } else {
-    this.redirect('/login');
-  }
-});
-
-router.get('/login', function *() {
-  if (this.req.isAuthenticated()) {
-    this.redirect('/dashboard');
-  } else {
     yield this.render('index');
   }
 });
 
 router.get('/dashboard', function *() {
   if (!this.req.isAuthenticated()) {
-    this.redirect('/login');
+    this.redirect('/');
   } else {
-    yield this.render('index');
+    yield this.render('dashboard');
   }
 });
 
 router.get('/user-profile', function *() {
   if (!this.req.isAuthenticated()) {
-    this.redirect('/login');
+    this.redirect('/');
   } else {
-    yield this.render('index');
+    yield this.render('dashboard');
   }
 });
 
